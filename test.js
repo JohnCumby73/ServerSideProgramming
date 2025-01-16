15a4dcb309b3af3400ae21d30cde4af62832773e
JohnCumby73 <john.cumby.jc@gmail.com>
2025-01-16 11:01:40 -0330

    MongoDB Setup

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.send('Hello, World!');
 });
 
 const port = 3000;
-app.listen(port, () => {
-    console.log('Server is running at http://localhost:${port}');
-});
 
+mongoose.connect('mongodb://Student24:Student24@logan', {dbName: 'home24' })
+    .then(() => {
+        console.log("Connected to the database!");
+        app.listen(port, () => {
+            console.log("Example app listening on port ${port}");
+        });
+    })
+    .catch(() => {
+        console.log("Failed to connect to the database.");
+    });
